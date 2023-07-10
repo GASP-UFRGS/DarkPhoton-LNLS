c15f52f4c26cc9402c83a5c6b6b1db9545484d1f
Fernando Bianchi <78316565+Febianchi@users.noreply.github.com>
2023-07-10 20:34:54 -0300

    Added Diamond Target
    Added diamond target
    
    Added diamond target. Also made changes in generator.cc so it randomly generate particles in different coordinates relatively to the diamond's plane.
    
    Updated .gitignore

--- simulation/physics.cc
+++ simulation/physics.cc
@@ -4,6 +4,13 @@ MyPhysicsList::MyPhysicsList()
 {
 	RegisterPhysics (new G4EmStandardPhysics());
 	RegisterPhysics (new G4OpticalPhysics());
+
+	RegisterPhysics (new G4EmExtraPhysics());
+	RegisterPhysics (new G4DecayPhysics());
+	RegisterPhysics (new G4IonPhysics());
+	RegisterPhysics (new G4StoppingPhysics());
+	RegisterPhysics (new G4HadronElasticPhysics());
+	RegisterPhysics (new G4HadronElasticPhysicsHP());
 }
 
 MyPhysicsList::~MyPhysicsList()
