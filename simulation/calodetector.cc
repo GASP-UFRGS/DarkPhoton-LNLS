a7aec9ba24413b44a355654fb6205c2c777ea19c
Fernando Bianchi <78316565+Febianchi@users.noreply.github.com>
2023-12-02 23:15:48 -0300

    Changes in root file management

--- simulation/calodetector.cc
+++ simulation/calodetector.cc
@@ -9,34 +9,43 @@ MySensitiveDetector::~MySensitiveDetector()
 G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist)
 {
     G4Track *track = aStep->GetTrack();
-
     track->SetTrackStatus(fStopAndKill);
-    
-    if (track->GetDefinition() == G4Gamma::Definition()) {
-        G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
-        G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
 
-        G4double energy = preStepPoint->GetTotalEnergy();
-        G4ThreeVector posParticle = preStepPoint->GetPosition();
+    G4cout << "TRACK IS: " << track->GetDefinition() << G4endl;
+    G4cout << "G4GAMMA IS: " << G4Gamma::Definition() << G4endl;
+    G4int PDGCode = track->GetDynamicParticle()->GetPDGcode();
+    G4cout << "PDGCODE IS: " << PDGCode << G4endl;
 
-        G4ThreeVector detectorPos = G4ThreeVector(0, 0, 3*m);
-        G4ThreeVector newPos = posParticle - detectorPos;
+    G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
+    G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
 
-        G4cout << "Photon position is: " << posParticle << G4endl;
-        G4cout << "Photon energy is: " << energy << G4endl;
+    G4double energy = preStepPoint->GetTotalEnergy();
+    G4ThreeVector posParticle = preStepPoint->GetPosition();
 
-        G4int evt = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
+    G4ThreeVector detectorPos = G4ThreeVector(0, 0, 3*m);
+    G4ThreeVector newPos = posParticle - detectorPos;
 
-        auto *man = G4RootAnalysisManager::Instance();
+    G4cout << "Particle position in calodetector is: " << posParticle << G4endl;
+    G4cout << "Particle energy in calodetector is: " << energy << G4endl;
 
-        man->FillNtupleIColumn(0, 0, evt);
+    G4int evt = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
+    G4cout << "\n\n EVENT ID IS: " << evt << "\n\n" << G4endl;
+
+    auto *man = G4RootAnalysisManager::Instance();
+    man->FillNtupleIColumn(0, 0, evt);
+
+    if (PDGCode == 22) {
         man->FillNtupleDColumn(0, 1, energy);
         man->FillNtupleDColumn(0, 2, posParticle[0]);
         man->FillNtupleDColumn(0, 3, posParticle[1]);
 
-        if (energy <= 100*MeV) {
-            man->FillNtupleDColumn(0, 4, energy);
-        }
-        man->AddNtupleRow(0);
+    } else {
+        man->FillNtupleDColumn(0, 1, -999);
+        man->FillNtupleDColumn(0, 2, -999);
+        man->FillNtupleDColumn(0, 3, -999);
     }
+    man->AddNtupleRow(0);
+
+    man->FillNtupleIColumn(2, 0, evt);
+    man->AddNtupleRow(2);
 }
--- simulation/run.cc
+++ simulation/run.cc
@@ -12,21 +12,26 @@ void MyRunAction::BeginOfRunAction(const G4Run*)
     man->OpenFile("output.root");
 
     man->CreateNtuple("PhotonHits", "PhotonHits");
-    man->CreateNtupleIColumn("fEvent");
+    man->CreateNtupleIColumn("EventID");
     man->CreateNtupleDColumn("PhotonEnergy");
     man->CreateNtupleDColumn("PhotonX");
     man->CreateNtupleDColumn("PhotonY");
-    man->CreateNtupleDColumn("newPhotonEnergy");
 
     man->FinishNtuple(0);
 
     man->CreateNtuple("PositronHits", "PositronHits");
-    man->CreateNtupleIColumn("fEvent");
+    man->CreateNtupleIColumn("EventID");
     man->CreateNtupleDColumn("PositronEnergy");
     man->CreateNtupleDColumn("PositronX");
     man->CreateNtupleDColumn("PositronY");
 
     man->FinishNtuple(1);
+
+    man->CreateNtuple("Events", "Events");
+    man->CreateNtupleIColumn("PhotonEvent");
+    man->CreateNtupleIColumn("PositronEvent");
+
+    man->FinishNtuple(2);
 }
 
 void MyRunAction::EndOfRunAction(const G4Run*)
